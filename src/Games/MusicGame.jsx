d0d1b7c0b504487acf88e5153f4316a5e808cd0d
Elián Fernández Labella <elian_f@hotmail.com>
2024-04-29 19:32:33 +0200

    (Sound) Keybindings, clicks. (Style) Piano.

--- src/Games/MusicGame.jsx
+++ src/Games/MusicGame.jsx
@@ -1,23 +1,90 @@
 import React, { useState, useEffect } from "react";
 
 import * as Tone from "tone";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
-
 import PianoTiles from "../components/PianoTiles";
-
 import "../index.css";
+
 
 const MusicGame = () => {
   const [sequence, setSequence] = useState([]);
+  const [currentNoteIndex, setCurrentNoteIndex] = useState(0);
+  const [currentNote, setCurrentNote] = useState(null);
 
   useEffect(() => {
     generateSequence();
   }, []);
 
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case 'a':
+        playNote("C4");
+        break;
+      case "s":
+        playNote("D4");
+        break;
+      case "d":
+        playNote("E4");
+        break;
+      case "f":
+        playNote("F4");
+        break;
+      case "g":
+        playNote("G4");
+        break;
+      case "h":
+        playNote("A4");
+        break;
+      case "j":
+        playNote("B4");
+        break;
+      case "k":
+        playNote("C5");
+        break;
+      case "l":
+        playNote("D5");
+        break;
+      case "ñ":
+        playNote("E5");
+        break;
+      case "w":
+        playNote("C#4");
+        break;
+      case "e":
+        playNote("D#4");
+        break;
+      case "t":
+        playNote("F#4");
+        break;
+      case "y":
+        playNote("G#4");
+        break;
+      case "u":
+        playNote("A#4");
+        break;
+      case "o":
+        playNote("C#5");
+        break;
+      case "p":
+        playNote("D#5");
+        break;
+      default:
+        break;
+    }
+  };
+
   // function to generate a random sequence of notes
   const generateSequence = () => {
-    const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
+    const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "C#4", "D#4", "F#4", "G#4", "A#4", "C#5", "D#5"];
     const newSequence = Array.from(
       { length: 4 },
       () => notes[Math.floor(Math.random() * notes.length)]
@@ -27,23 +94,110 @@ const MusicGame = () => {
 
   // function to play the sequence generated before
   const playSequence = () => {
-    generateSequence();
     console.log(sequence);
     const synth = new Tone.Synth().toDestination();
     sequence.forEach((note, index) => {
-      synth.triggerAttackRelease(note, "8n", Tone.now() + index * 0.5);
+      setTimeout(() => {
+        setCurrentNote(note);
+        synth.triggerAttackRelease(note, "8n");
+      }, index * 500);
     });
+
+    setTimeout(() => {
+      setCurrentNote(null);
+    }, sequence.length * 500);
+
   };
 
-  // function to check if the user's input matches the sequence
-  const checkSequence = (input) => {
-    if (input.join("") === sequence.join("")) {
-      alert("Correct!");
-      generateSequence();
-    } else {
-      alert("Incorrect!");
+  const playNote = (note) => {
+    switch (note) {
+      case "C4":
+        new Tone.Synth().toDestination().triggerAttackRelease("C4", "8n");
+        break;
+      case "D4":
+        new Tone.Synth().toDestination().triggerAttackRelease("D4", "8n");
+        break;
+      case "E4":
+        new Tone.Synth().toDestination().triggerAttackRelease("E4", "8n");
+        break;
+      case "F4":
+        new Tone.Synth().toDestination().triggerAttackRelease("F4", "8n");
+        break;
+      case "G4":
+        new Tone.Synth().toDestination().triggerAttackRelease("G4", "8n");
+        break;
+      case "A4":
+        new Tone.Synth().toDestination().triggerAttackRelease("A4", "8n");
+        break;
+      case "B4":
+        new Tone.Synth().toDestination().triggerAttackRelease("B4", "8n");
+        break;
+      case "C5":
+        new Tone.Synth().toDestination().triggerAttackRelease("C5", "8n");
+        break;
+      case "D5":
+        new Tone.Synth().toDestination().triggerAttackRelease("D5", "8n");
+        break;
+      case "E5":
+        new Tone.Synth().toDestination().triggerAttackRelease("E5", "8n");
+        break;
+      case "C#4":
+        new Tone.Synth().toDestination().triggerAttackRelease("C#4", "8n");
+        break;
+      case "D#4":
+        new Tone.Synth().toDestination().triggerAttackRelease("D#4", "8n");
+        break;
+      case "F#4":
+        new Tone.Synth().toDestination().triggerAttackRelease("F#4", "8n");
+        break;
+      case "G#4":
+        new Tone.Synth().toDestination().triggerAttackRelease("G#4", "8n");
+        break;
+      case "A#4":
+        new Tone.Synth().toDestination().triggerAttackRelease("A#4", "8n");
+        break;
+      case "C#5":
+        new Tone.Synth().toDestination().triggerAttackRelease("C#5", "8n");
+        break;
+      case "D#5":
+        new Tone.Synth().toDestination().triggerAttackRelease("D#5", "8n");
+        break;
+      default:
+        break;
     }
   };
+
+
+  // function to check if the user's input matches the sequence
+  const checkSequence = (note) => {
+    
+    //console.log(sequence[currentNoteIndex]);
+    if (note === sequence[currentNoteIndex]) {
+      // La nota es correcta, pasa a la siguiente nota
+      setCurrentNoteIndex(currentNoteIndex + 1);
+
+      if (currentNoteIndex + 1 === sequence.length) {
+        // La secuencia completa fue tocada correctamente
+        console.log('Sequence completed!');
+        // Aquí puedes hacer algo para indicar que la secuencia fue tocada correctamente
+        alert("Secuencia completada correctamente");
+        generateSequence();
+      }
+    } else {
+      // La nota es incorrecta, reinicia la secuencia
+      console.log('Incorrect note!');
+      setCurrentNoteIndex(0);
+      // Aquí puedes hacer algo para indicar que la nota fue incorrecta
+    }
+  };
+
+  const handleUserInput = (note) => {
+    // se ha reproducido la secuncia generada, ahora el usuario debera repetirla en el piano y se comprobara si es correcta
+    playNote(note);
+    console.log("Nota clickada: ", note);
+    console.log("Orden de la secuencia a repetir: ", sequence[currentNoteIndex]);
+    checkSequence(note);
+  }
 
   return (
     <>
@@ -70,10 +224,10 @@ const MusicGame = () => {
           {/* PianoTiles */}
           <div className="border-2 border-black rounded-xl p-4 bg-black">
             <p className="text-white font-bold text-center mb-4">
-              Playable piano
+              Piano
             </p>
 
-            <PianoTiles />
+            <PianoTiles handleTileClick={handleUserInput} currentNote={currentNote} />
           </div>
         </div>
       </div>
--- src/components/PianoTiles.jsx
+++ src/components/PianoTiles.jsx
@@ -1,16 +1,48 @@
 import React from "react";
+import * as Tone from "tone";
 
-const WhiteTile = () => {
+const notesWhite = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5"];
+const notesBlack = ["C#4", "D#4", "F#4", "G#4", "A#4", "C#5", "D#5"];
+const synth = new Tone.Synth().toDestination();
+
+const teclas = {
+  'C4': 'A',
+  'D4': 'S',
+  'E4': 'D',
+  'F4': 'F',
+  'G4': 'G',
+  'A4': 'H',
+  'B4': 'J',
+  'C5': 'K',
+  'D5': 'L',
+  'E5': 'Ñ',
+  'C#4': 'W',
+  'D#4': 'E',
+  'F#4': 'T',
+  'G#4': 'Y',
+  'A#4': 'U',
+  'C#5': 'O',
+  'D#5': 'P'
+};
+
+
+const WhiteTile = ({ onClick, note, letter }) => {
   return (
-    <button className="z-1 w-8 h-44 md:w-16 md:h-50 lg:w-24 lg:h-68 bg-white rounded-md rounded-t-none mr-1 cursor-pointer hover:bg-gray-300 focus:outline-none first:rounded-t-md last:rounded-t-md" />
+    <button className="z-1 w-8 h-44 md:w-16 md:h-50 lg:w-24 lg:h-68 bg-white rounded-md rounded-t-none mr-1 cursor-pointer hover:bg-gray-300 focus:outline-none first:rounded-t-md last:rounded-t-md" onClick={() => onClick(note)}>
+      {letter}
+    </button>
   );
 };
 
-const BlackTile = ({ responsiveData }) => {
-  return <button className={`absolute ${responsiveData} bg-black rounded-md rounded-t-none hover:bg-blue-300`}/>;
+const BlackTile = ({ responsiveData, onClick, note, letter}) => {
+  return (
+    <button className={`text-white absolute ${responsiveData} bg-black rounded-md rounded-t-none hover:bg-blue-300`} onClick={() => onClick(note)}>
+    {letter}
+    </button>
+  );
 };
 
-const PianoTiles = () => {
+const PianoTiles = ({handleTileClick}) => {
   const whiteTiles = new Array(10).fill(0);
   const blackTiles = new Array(7).fill(0);
   const blackTilesPos = [
@@ -28,12 +60,12 @@ const PianoTiles = () => {
       <div id="keys" className="flex relative">
         {/* White tiles */}
         {whiteTiles.map((_, index) => (
-          <WhiteTile key={index} />
+          <WhiteTile key={index} note={notesWhite[index]} onClick={handleTileClick} letter={teclas[notesWhite[index]]} />
         ))}
 
         {/* Black tiles */}
         {blackTiles.map((_, index) => (
-            <BlackTile key={index} responsiveData={blackTilesPos[index]} />
+          <BlackTile key={index} note={notesBlack[index]} onClick={handleTileClick} letter={teclas[notesBlack[index]]} responsiveData={blackTilesPos[index]} />
         ))}
       </div>
     </>
